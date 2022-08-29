f75dadee7a1083f5f96753884b43d34f840a5336
gepz <62634767+gepz@users.noreply.github.com>
2022-08-29 22:54:49 +0800

    docs(assert): disable a eslint rule for a line

--- packages/assert/src/index.ts
+++ packages/assert/src/index.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line max-len
+// eslint-disable-next-line consistent-default-export-name/default-export-match-filename
 import {
   assert as checkAssert,
 } from 'check-types';
