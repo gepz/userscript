d20916a909e3ca096db09f0e043c88f095832b11
gepz <62634767+gepz@users.noreply.github.com>
2022-09-24 20:29:53 +0800

    build: new package: image-to-text(itt)

---
+++ packages/image-to-text/config/webpack.base.conf.ts
@@ -0,0 +1,19 @@
+import styleLoaderConfig from '@userscript/webpack-config/styleLoaderConfig';
+import tsbaseConfig from '@userscript/webpack-config/tsbaseConfig';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import {
+  merge,
+} from 'webpack-merge';
+
+export default merge(
+  tsbaseConfig(path.join(__dirname, '../')),
+  styleLoaderConfig,
+  {
+    plugins: [
+      new ForkTsCheckerWebpackPlugin(),
+      new HtmlWebpackPlugin(),
+    ],
+  },
+);
