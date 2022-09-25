ce512410ab16b1de76a8a1e1d3e14dd001bcbb4d
gepz <62634767+gepz@users.noreply.github.com>
2022-09-25 14:54:25 +0800

    build(itt): remove externals

--- packages/image-to-text/config/webpack.prod.conf.ts
+++ packages/image-to-text/config/webpack.prod.conf.ts
@@ -11,21 +11,7 @@ import webpackBaseConf from './webpack.base.conf';
 export default merge(
   webpackBaseConf,
   {
-    externals: [
-      {
-        sweetalert2: 'Swal',
-        rxjs: 'rxjs',
-        'feather-icons': 'feather',
-        loglevel: 'log',
-        mithril: 'm',
-        'check-types': 'check',
-        'deep-diff': 'DeepDiff',
-        astring: 'astring',
-        jsep: 'jsep',
-        'hash-it': 'window[\'hash-it\']',
-        'micro-memoize': 'window[\'micro-memoize\']',
-      },
-    ],
+    externals: [],
     plugins: [
       new BundleAnalyzerPlugin({
         analyzerMode: 'static',
