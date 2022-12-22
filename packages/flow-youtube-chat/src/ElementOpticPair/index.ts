6e50512615d58d21de552dd6bae16f88d1e60025
gepz <62634767+gepz@users.noreply.github.com>
2022-12-22 23:52:23 +0800

    refactor(fyc): minor refactors

--- packages/flow-youtube-chat/src/ElementOpticPair/index.ts
+++ packages/flow-youtube-chat/src/ElementOpticPair/index.ts
@@ -1,4 +1,5 @@
 import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
 import {
   Refinement,
 } from 'fp-ts/Refinement';
@@ -12,10 +13,14 @@ export type ElementOpticPair<S, A> = {
   opt: Op.Optional<S, A>
 };
 
-export const of = <A>(ele: A) => <S>(opt: Op.Optional<S, A>) => ({
+export const make = <A>(ele: A) => <S>(
+  opt: Op.Optional<S, A>,
+): ElementOpticPair<S, A> => ({
   ele,
   opt,
 });
+
+export const of = <A>(ele: A): ElementOpticPair<A, A> => make(ele)(Op.id());
 
 export const filter = <S, A, B extends A>(
   refinement: Refinement<A, B>,
@@ -42,3 +47,27 @@ export const prop = <A, P extends keyof A>(p: P) => <S>(
     Op.prop(p),
   ),
 });
+
+export const toArray = <S, A>(
+  pair: ElementOpticPair<S, readonly A[]>,
+): readonly ElementOpticPair<S, A>[] => pipe(
+  pair.ele,
+  RA.mapWithIndex((i, e) => pipe(
+    pair.opt,
+    Op.index(i),
+    make(e),
+  )),
+);
+
+export const some = <S, A>(
+  pair: ElementOpticPair<S, O.Option<A>>,
+): O.Option<ElementOpticPair<S, A>> => pipe(
+  pair.ele,
+  O.map((x) => ({
+    ele: x,
+    opt: pipe(
+      pair.opt,
+      Op.some,
+    ),
+  })),
+);
