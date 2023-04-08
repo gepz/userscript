2c2d07718f83ff53bf3f74f4ac2d3af6617c2a2f
gepz <62634767+gepz@users.noreply.github.com>
2023-04-08 18:07:48 +0800

    build: remove cdn-from-dependency

--- packages/assert/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare const assert: (value: unknown, message?: string) => asserts value;
-export default assert;
-//# sourceMappingURL=index.d.ts.map
--- packages/assert/lib/index.js
+++
@@ -1,4 +0,0 @@
-import { assert as checkAssert, } from 'check-types';
-const assert = checkAssert;
-export default assert;
-//# sourceMappingURL=index.js.map
