0d76438e475d05edbd76248806141f6b56e49e3a
gepz <62634767+gepz@users.noreply.github.com>
2024-03-16 02:14:24 +0800

    fix(fyc): chat now remove when interval too small
    Fix a bug where a chat would not be removed properly
     when the lane interval became too small during animation
     , typically when user adjusted settings.

--- packages/flow-youtube-chat/src/setChatAnimation/index.ts
+++ packages/flow-youtube-chat/src/setChatAnimation/index.ts
@@ -5,19 +5,17 @@ import {
 } from 'effect/Function';
 import * as O from 'effect/Option';
 import * as RA from 'effect/ReadonlyArray';
-import * as tuple from 'effect/Tuple';
 import hash from 'hash-it';
 import memoize from 'micro-memoize';
 
 import FlowChat from '@/FlowChat';
 import MainState from '@/MainState';
-import flowDuration from '@/flowDuration';
 import getChatFontSize from '@/getChatFontSize';
 import getChatLane from '@/getChatLane';
 import getFlowChatProgress from '@/getFlowChatProgress';
-import getLaneY from '@/getLaneY';
 import intervalTooSmall from '@/intervalTooSmall';
 import setChatPlayState from '@/setChatPlayState';
+import setNewChatAnimation from '@/setNewChatAnimation';
 
 const getWidth = memoize(
   (ele: Element | null): number => ele?.getBoundingClientRect().width ?? 0,
@@ -46,106 +44,39 @@ export default (
     () => !chat.animationEnded,
     () => new Cause.NoSuchElementException(),
   ),
-  Z.map((height) => (
-    {
-      newChat: {
-        ...chat,
-        width: getWidth(chat.element.firstElementChild),
-        height,
-      } satisfies FlowChat,
-      oldChatIndex: pipe(
-        mainState.flowChats.value,
-        RA.findFirstIndex((x) => x === chat),
-      ),
-      progress: getFlowChatProgress(chat.animation),
-    })),
-  Z.map((ctx) => pipe(
+  Z.map((height) => ({
+    newChat: {
+      ...chat,
+      width: getWidth(chat.element.firstElementChild),
+      height,
+    } satisfies FlowChat,
+    oldChatIndex: pipe(
+      mainState.flowChats.value,
+      RA.findFirstIndex((x) => x === chat),
+    ),
+    progress: getFlowChatProgress(chat.animation),
+  })),
+  Z.flatMap((ctx) => pipe(
     getChatLane(ctx.newChat, ctx.oldChatIndex, ctx.progress)(mainState),
-    (x) => ({
-      newChat: {
-        ...ctx.newChat,
-        lane: x.lane,
-      } satisfies FlowChat,
-      oldChatIndex: ctx.oldChatIndex,
-      currentTime: ctx.progress * flowDuration,
-      intervalTooSmall: intervalTooSmall(x.interval)(mainState.config.value),
-    }),
-  )),
-  Z.filterOrElse(
-    (x) => !x.intervalTooSmall,
-    (ctx) => pipe(
-      ctx.newChat.animation,
-      Z.tap((x) => Z.sync(() => x.finish())),
-      Z.map((): typeof ctx => ({
+    ({
+      lane, interval,
+    }) => pipe(
+      intervalTooSmall(interval)(mainState.config.value)
+        ? pipe(
+          ctx.newChat.animation,
+          Z.tap((x) => Z.sync(() => x.finish())),
+          Z.map((): FlowChat => ({
+            ...ctx.newChat,
+            animation: O.none(),
+          })),
+          Z.orElse(() => Z.succeed<FlowChat>(ctx.newChat)),
+        ) : setNewChatAnimation(ctx.newChat)(lane)(ctx.progress)(mainState),
+      Z.map((x): typeof ctx => ({
         ...ctx,
-        newChat: {
-          ...ctx.newChat,
-          animation: O.none(),
-        },
+        newChat: x,
       })),
     ),
-  ),
-  Z.map((x): typeof x => ({
-    ...x,
-    newChat: {
-      ...x.newChat,
-      y: getLaneY(x.newChat.lane, mainState),
-    },
-  })),
-  Z.tap((ctx) => pipe(
-    ctx.newChat.animation,
-    O.match(({
-      onNone: () => Z.unit,
-      onSome: (x) => Z.sync(() => x.cancel()),
-    })),
   )),
-  Z.flatMap((ctx) => pipe(
-    [
-      [
-        mainState.playerRect.value.width
-          * (mainState.config.value.flowX2 - mainState.config.value.flowX1),
-        ctx.newChat.y,
-      ],
-      [
-        -ctx.newChat.width,
-        ctx.newChat.y,
-      ],
-    ] as const,
-    RA.map(pipe(
-      (x: number) => `${x}px`,
-      (x) => tuple.mapBoth({
-        onFirst: x,
-        onSecond: x,
-      }),
-    )),
-    RA.map((([x, y]) => ({
-      transform: `translate(${x}, ${y})`,
-    }))),
-    (x) => Z.sync(() => ctx.newChat.element.animate(x, {
-      duration: flowDuration,
-      easing: mainState.config.value.timingFunction,
-    })),
-    Z.flatMap((animation) => Z.sync((): FlowChat => {
-      const newChat = {
-        ...ctx.newChat,
-        animation: O.some(animation),
-      };
-
-      Object.assign(animation, {
-        onfinish: () => Object.assign(newChat, {
-          animationEnded: true,
-        } satisfies Partial<FlowChat>),
-        currentTime: ctx.currentTime,
-      } satisfies Partial<Animation>);
-
-      return newChat;
-    })),
-    Z.map((newChat) => ({
-      oldChatIndex: ctx.oldChatIndex,
-      newChat,
-    })),
-  )),
-  Z.tap((x) => setChatPlayState(x.newChat)(mainState)),
   Z.flatMap((x) => O.match(x.oldChatIndex, {
     onNone: () => Z.succeed({
       newChat: x.newChat,
@@ -157,5 +88,5 @@ export default (
       Z.zipRight(Z.fail(new Cause.NoSuchElementException())),
     ),
   })),
+  Z.tap((x) => setChatPlayState(x.newChat)(mainState)),
 );
-
