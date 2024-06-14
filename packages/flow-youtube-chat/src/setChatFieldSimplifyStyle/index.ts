2670a1d0d8c97aa41863e65ff31d1b28e4bcb967
gepz <62634767+gepz@users.noreply.github.com>
2024-06-14 09:01:42 +0800

    refactor(fyc): minor refators

--- packages/flow-youtube-chat/src/setChatFieldSimplifyStyle/index.ts
+++ packages/flow-youtube-chat/src/setChatFieldSimplifyStyle/index.ts
@@ -7,23 +7,28 @@ import {
 
 export default (
   chat: HTMLElement,
-): Z.Effect<void> => (chat.querySelector(
-  '.style-scope.yt-live-chat-paid-message-renderer',
-) ? Z.void
-: pipe(
-  [
-    '#author-photo',
-    'yt-live-chat-author-chip.style-scope.yt-live-chat-text-message-renderer',
-  ],
-  A.map((x) => O.fromNullable(chat.querySelector<HTMLElement>(x))),
-  A.getSomes,
-  A.map((x) => Z.sync(() => {
-    // eslint-disable-next-line no-param-reassign
-    x.style.display = 'none';
-  })),
-  A.append(Z.sync(() => {
-    // eslint-disable-next-line no-param-reassign
-    chat.style.borderBottom = '1px solid var(--yt-spec-text-secondary)';
-  })),
-  Z.all,
-));
+// eslint-disable-next-line func-names
+): Z.Effect<void> => Z.gen(function* () {
+  if (!chat.querySelector(
+    '.style-scope.yt-live-chat-paid-message-renderer',
+  )) {
+    yield* pipe(
+      [
+        '#author-photo',
+        // eslint-disable-next-line max-len
+        'yt-live-chat-author-chip.style-scope.yt-live-chat-text-message-renderer',
+      ],
+      A.map((x) => O.fromNullable(chat.querySelector<HTMLElement>(x))),
+      A.getSomes,
+      A.map((x) => Z.sync(() => {
+        // eslint-disable-next-line no-param-reassign
+        x.style.display = 'none';
+      })),
+      A.append(Z.sync(() => {
+        // eslint-disable-next-line no-param-reassign
+        chat.style.borderBottom = '1px solid var(--yt-spec-text-secondary)';
+      })),
+      Z.all,
+    );
+  }
+});
